c49fc4a16108fa6294349d4793a687fd7956e2c2
Jesus Torres <jtorres122436@gmail.com>
2021-05-04 00:17:07 -0500

    With the pathing fixed I was able to actually flesh out the calculating aspects of the code.     Month count code was mostly unchanged from early ideation.     Net total sum code was mostly unchanged from early ideation.     Greatest increase/decrease of profit, however, had its syntax cleaned up and fleshed out to properly work. Was able to begin calculating averge rate of change.     Started out thinking I needed to keep track of each interval of change, but then I realized each individual interval doesn't matter.     Only thing that mattered was the final and intial. All the calcultions seem to work for the general case. I just need to improve formatting and export it to a text file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,19 +10,31 @@ mnthcount = 0
 #Net total sum storer
 nettotal = 0
 #Greatest Increase in Profits storer
-grtstgain = []
+grtstgain = 0
 #Greatest Decrease in Profits storer
-grtstlss = []
+grtstlss = 0
+#Previous Profit/Loss
+prevprft=0
 
 with open(budgetpath) as csvfile:
-    budgetreader = csv.reader(budgetpath,delimiter=',')
+    budgetreader = csv.reader(csvfile,delimiter=',')
     budget_header = next(budgetreader)
     for row in budgetreader:
-        print(row)
-    #    mnthcount = mnthcount + 1
-    #    nettotal = nettotal + int(row[0])
-    #    if row - grtstgain > 0:
-    #        grtstgain = row
-    #    if grtstlss - row > 0:
-    #        grtstlss = row
-#print(f"Financial Analysis\n----------------------------\nTotal Months: {mnthcount}\nTotal: ${nettotal}\nGreatest Increase in Profits: {grtstgain[0]} (${grtstgain[1]})\nGreatest Decrease in Profits: {grtstlss[0]} (${grtstlss[1]})")
+        mnthcount = mnthcount + 1
+        nettotal = nettotal + int(row[1])
+        if mnthcount == 1:
+            prft0 = int(row[1])
+        else:
+            prftf = int(row[1])
+        if int(row[1]) - prevprft > grtstgain:
+            grtstgain = int(row[1])-prevprft
+        if grtstlss > int(row[1]) - prevprft:
+            grtstlss = int(row[1])-prevprft
+        prevprft = int(row[1])
+    avrgdelta = (prftf - prft0) / (mnthcount - 1)
+#print(f"Financial Analysis\n----------------------------\nTotal Months: {mnthcount}\nGreatest Increase in Profits: {grtstgain[0]} (${grtstgain[1]})\nGreatest Decrease in Profits: {grtstlss[0]} (${grtstlss[1]})")
+print(mnthcount)
+print(nettotal)
+print(grtstgain)
+print(grtstlss)
+print(avrgdelta)
