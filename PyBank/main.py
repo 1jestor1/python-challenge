3d586d12c20f8ade36601c06b9b895c2470b4d92
Jesus Torres <jtorres122436@gmail.com>
2021-05-03 22:03:10 -0500

    Technically I just fixed the VSCode settings to actually use my current working directory.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,28 @@
 import os
 import csv
 
-#WHY ARE YOU NOT WORKING?
+#WHY ARE YOU NOT WORKING?!? 
 budgetpath = os.path.join('Resources','budget_data.csv')
+print(budgetpath)
 #outputpath = os.path.join('Output','budget_analysis.csv')
-mnthcount = 0
-nettotal = 0
-grtstgain = []
-grtstlss = []
+#Month Count
+#mnthcount = 0
+#Net total sum storer
+#nettotal = 0
+#Greatest Increase in Profits storer
+#grtstgain = []
+#Greatest Decrease in Profits storer
+#grtstlss = []
 
-with open(budgetpath) as csvfile:
-    budgetreader = csv.reader(budgetpath,delimiter=',')
-    budget_header = next(budgetreader)
-    for row in budgetreader:
-        mnthcount = mnthcount + 1
-        nettotal = nettotal + row[1]
-        if row - grtstgain > 0:
-            grtstgain = row
-        if grtstlss - row > 0:
-            grtstlss = row
-print(f"Financial Analysis\n----------------------------\nTotal Months: {mnthcount}\nTotal: ${nettotal}\nGreatest Increase in Profits: {grtstgain[0]} (${grtstgain[1]})\nGreatest Decrease in Profits: {grtstlss[0]} (${grtstlss[1]})")
+#with open(budgetpath) as csvfile:
+#    budgetreader = csv.reader(budgetpath,delimiter=',')
+#    budget_header = next(budgetreader)
+#    for row in budgetreader:
+#        print(row)
+#        mnthcount = mnthcount + 1
+#        nettotal = nettotal + int(row[1])
+#        if row - grtstgain > 0:
+#            grtstgain = row
+#        if grtstlss - row > 0:
+#            grtstlss = row
+#print(f"Financial Analysis\n----------------------------\nTotal Months: {mnthcount}\nTotal: ${nettotal}\nGreatest Increase in Profits: {grtstgain[0]} (${grtstgain[1]})\nGreatest Decrease in Profits: {grtstlss[0]} (${grtstlss[1]})")
