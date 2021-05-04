d436f7880a9f35098cb2f4a2b1fb898b21c7c748
Jesus Torres <jtorres122436@gmail.com>
2021-05-03 22:54:51 -0500

    Nevermind. Something else is broken. Seems to be reading my file path as a string.     Should be reading as 'Resources/budget_data.csv'     Instead we are getting 'Resources\\budget_data.csv'

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,23 +6,23 @@ budgetpath = os.path.join('Resources','budget_data.csv')
 print(budgetpath)
 #outputpath = os.path.join('Output','budget_analysis.csv')
 #Month Count
-#mnthcount = 0
+mnthcount = 0
 #Net total sum storer
-#nettotal = 0
+nettotal = 0
 #Greatest Increase in Profits storer
-#grtstgain = []
+grtstgain = []
 #Greatest Decrease in Profits storer
-#grtstlss = []
+grtstlss = []
 
-#with open(budgetpath) as csvfile:
-#    budgetreader = csv.reader(budgetpath,delimiter=',')
-#    budget_header = next(budgetreader)
-#    for row in budgetreader:
-#        print(row)
-#        mnthcount = mnthcount + 1
-#        nettotal = nettotal + int(row[1])
-#        if row - grtstgain > 0:
-#            grtstgain = row
-#        if grtstlss - row > 0:
-#            grtstlss = row
+with open(budgetpath) as csvfile:
+    budgetreader = csv.reader(budgetpath,delimiter=',')
+    budget_header = next(budgetreader)
+    for row in budgetreader:
+        print(row)
+    #    mnthcount = mnthcount + 1
+    #    nettotal = nettotal + int(row[0])
+    #    if row - grtstgain > 0:
+    #        grtstgain = row
+    #    if grtstlss - row > 0:
+    #        grtstlss = row
 #print(f"Financial Analysis\n----------------------------\nTotal Months: {mnthcount}\nTotal: ${nettotal}\nGreatest Increase in Profits: {grtstgain[0]} (${grtstgain[1]})\nGreatest Decrease in Profits: {grtstlss[0]} (${grtstlss[1]})")
