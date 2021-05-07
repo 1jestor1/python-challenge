2f4a8f465418ec60d2857a1442e975025f43c8d5
Jesus Torres <jtorres122436@gmail.com>
2021-05-06 22:40:53 -0500

    Finished all calculations. Only clean up now.     Fixed the vote count to actually count votes and not add voter ids.     Made it so the summary is output to terminal and a text file in required formatt.         Since I'm trying to make this as general as possible. I had to figure out how to make the message dependent on the number of candidates.         A loop seemed like the best way to do this.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import csv
 #path to election data in a [vote count, "county", "candidate"]
 lctnData_path = os.path.join("Resources","election_data.csv")
 #path to output data
-lctnResults_path = os.path.join("Output","election_results.txt")
+outputpath = os.path.join("Output","election_results.txt")
 #total vote count
 totalvts = 0
 #List of candidates.
@@ -14,12 +14,15 @@ cnddtVtList = []
 uniCnddtCheck = True
 #Candidate place value
 cnddtListIndex = 0
-
+#Winner count for comparisons
+vtWnnrCount = 0
+#Winer index for storing winner.
+vtWnnrIndex = 0
 with open(lctnData_path) as lctnData_file:
     lctnReader = csv.reader(lctnData_file, delimiter=',')
     lctnHeader = next(lctnReader)
     for row in lctnReader:
-        totalvts = totalvts + int(row[0])
+        totalvts = totalvts + 1
         #Want to check for unique candidates and to update both lists when necessary.
         for candidate in cnddtList:
             if row[2] == candidate:
@@ -27,10 +30,19 @@ with open(lctnData_path) as lctnData_file:
                 cnddtListIndex = cnddtList.index(candidate)
         if uniCnddtCheck == True:
             cnddtList.append(row[2])
-            cnddtVtList.append(int(row[0]))
+            cnddtVtList.append(1)
         else:
-            cnddtVtList[cnddtListIndex]=cnddtVtList[cnddtListIndex]+int(row[0])
+            cnddtVtList[cnddtListIndex]=cnddtVtList[cnddtListIndex] + 1
             uniCnddtCheck = True     
-print(cnddtList)
-print(cnddtVtList)          
-print(totalvts)
+for votecount in cnddtVtList:
+    if vtWnnrCount < votecount:
+        vtWnnrCount = votecount
+        vtWnnrIndex = cnddtVtList.index(votecount)
+summary = f"Election Results\n-------------------------\nTotal Votes: {totalvts}\n-------------------------\n"
+for candidate in cnddtList:
+    percent = "{:.3f}%".format(cnddtVtList[cnddtList.index(candidate)] / totalvts * 100)
+    summary = summary + f"{candidate}: {percent} ({cnddtVtList[cnddtList.index(candidate)]})\n"
+summary = summary + f"-------------------------\nWinner: {cnddtList[vtWnnrIndex]}\n-------------------------"
+print(summary)
+with open(outputpath, "w") as smmryfile:
+    smmryfile.write(summary)
